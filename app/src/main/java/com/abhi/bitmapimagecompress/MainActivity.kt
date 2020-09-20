892e01d3310cf1559fa09851fafc7b29b1873537
Abhishek Srivastava <abhisheksrvstv7@gmail.com>
2020-09-20 12:29:48 +0530

    Compressed Image Size issue

--- app/src/main/java/com/abhi/bitmapimagecompress/MainActivity.kt
+++ app/src/main/java/com/abhi/bitmapimagecompress/MainActivity.kt
@@ -1,38 +1,330 @@
 package com.abhi.bitmapimagecompress
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.*
+import android.content.pm.PackageManager
+import android.database.Cursor
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
+import android.os.Environment
+import android.provider.DocumentsContract
+import android.provider.MediaStore
+import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import kotlinx.android.synthetic.main.content_main.*
+import java.io.*
+
 
 class MainActivity : AppCompatActivity() {
 
+    private val PERMISSION_CODE = 1000;
+    private val IMAGE_CAPTURE_CODE = 1001
+    var image_uri: Uri? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(findViewById(R.id.toolbar))
 
         findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+            capturePhoto()
         }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
+    fun capturePhoto()
+    {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            if (checkSelfPermission(Manifest.permission.CAMERA)
+                == PackageManager.PERMISSION_DENIED ||
+                checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                == PackageManager.PERMISSION_DENIED){
+                //permission was not enabled
+                val permission = arrayOf(
+                    Manifest.permission.CAMERA,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+                )
+                //show popup to request permission
+                requestPermissions(permission, PERMISSION_CODE)
+            }
+            else{
+                //permission already granted
+                openCamera()
+            }
+        }
+        else{
+            //system os is < marshmallow
+            openCamera()
         }
     }
+
+    private fun openCamera() {
+        val values = ContentValues()
+        values.put(MediaStore.Images.Media.TITLE, "New Picture")
+        values.put(MediaStore.Images.Media.DESCRIPTION, "From the Camera")
+        image_uri = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
+        //camera intent
+        val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, image_uri)
+        startActivityForResult(cameraIntent, IMAGE_CAPTURE_CODE)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode){
+            PERMISSION_CODE -> {
+                if (grantResults.size > 0 && grantResults[0] ==
+                    PackageManager.PERMISSION_GRANTED
+                ) {
+                    //permission from popup was granted
+                    openCamera()
+                } else {
+                    //permission from popup was denied
+                    Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.KITKAT)
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (resultCode == Activity.RESULT_OK){
+
+            Old_Image_size.text=calculateFileSize(image_uri!!)+" MB"
+            show_pic.setImageURI(image_uri)
+
+            var uri=image_uri!!
+            val compressFilePath:File?=createCompressImage(File(getPathFromUri(this,uri)))
+
+            new_show_pic.setImageURI(Uri.parse(getPathFromUri(this,uri)))
+            new_Image_size.text=calculateFileSize(Uri.parse(compressFilePath?.path))+" MB"
+        }
+    }
+
+    //https://www.mindbowser.com/image-compression-in-android/
+    @SuppressLint("Recycle")
+    private fun  calculateFileSize(uri: Uri):String
+    {
+
+        val scheme = uri.scheme
+        println("Scheme type $scheme")
+        if (scheme == ContentResolver.SCHEME_CONTENT) {
+            try {
+                val fileInputStream = applicationContext.contentResolver.openInputStream(uri)
+                val fileSizeInBytes = fileInputStream!!.available()
+                val fileSizeInKB = fileSizeInBytes / 1024;
+                // Convert the KB to MegaBytes (1 MB = 1024 KBytes)
+                val fileSizeInMB = fileSizeInKB / 1024
+                return fileSizeInMB.toString();
+
+            } catch (e: java.lang.Exception) {
+                return "0"
+            }
+        } else if (scheme == ContentResolver.SCHEME_FILE) {
+            val path = uri.path
+            var f:File?=null
+            try {
+                f = File(path!!)
+            } catch (e: java.lang.Exception) {
+                e.printStackTrace()
+            }
+            System.out.println("File size in bytes" + f?.length())
+            val fileSizeInBytes = f?.length()
+            val fileSizeInKB = fileSizeInBytes?.div(1024)
+            // Convert the KB to MegaBytes (1 MB = 1024 KBytes)
+            val fileSizeInMB = fileSizeInKB?.div(1024)
+            return fileSizeInMB.toString();
+        }
+        return "0"
+
+    }
+
+
+
+
+    fun createCompressImage(destFile: File):File?
+    {
+        return decodeFile(destFile)
+    }
+
+    private fun decodeFile(f: File): File? {
+        var b: Bitmap? = null
+        var destFile:File?=null
+        //Decode image size
+        val o = BitmapFactory.Options()
+        o.inJustDecodeBounds = true
+        var fis: FileInputStream? = null
+        try {
+            fis = FileInputStream(f)
+            BitmapFactory.decodeStream(fis, null, o)
+            fis.close()
+        } catch (e: FileNotFoundException) {
+            e.printStackTrace()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+        val IMAGE_MAX_SIZE = 1024
+        var scale = 1
+        if (o.outHeight > IMAGE_MAX_SIZE || o.outWidth > IMAGE_MAX_SIZE) {
+            scale = Math.pow(
+                2.0, Math.ceil(
+                    Math.log(
+                        IMAGE_MAX_SIZE / Math.max(
+                            o.outHeight,
+                            o.outWidth
+                        ).toDouble()
+                    ) / Math.log(0.5)
+                ) as Double
+            ).toInt()
+        }
+
+        //Decode with inSampleSize
+        val o2 = BitmapFactory.Options()
+        o2.inSampleSize = scale
+        try {
+            fis = FileInputStream(f)
+            b = BitmapFactory.decodeStream(fis, null, o2)
+            fis.close()
+        } catch (e: FileNotFoundException) {
+            e.printStackTrace()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+        destFile = File(File(f.absolutePath), ("img_" + System.currentTimeMillis() + ".png"))
+        try {
+            val out = FileOutputStream(destFile)
+            b?.compress(Bitmap.CompressFormat.PNG, 100, out)
+            out.flush()
+            out.close()
+        } catch (e: java.lang.Exception) {
+            e.printStackTrace()
+        }
+        return destFile
+    }
+
+
+    @RequiresApi(Build.VERSION_CODES.KITKAT)
+    fun getPathFromUri(context: Context, uri: Uri): String? {
+        val isKitKat = Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT
+
+        // DocumentProvider
+        if (isKitKat && DocumentsContract.isDocumentUri(context, uri)) {
+            // ExternalStorageProvider
+            if (isExternalStorageDocument(uri)) {
+                val docId = DocumentsContract.getDocumentId(uri)
+                val split = docId.split(":".toRegex()).toTypedArray()
+                val type = split[0]
+                if ("primary".equals(type, ignoreCase = true)) {
+                    return Environment.getExternalStorageDirectory().toString() + "/" + split[1]
+                }
+
+                // TODO handle non-primary volumes
+            } else if (isDownloadsDocument(uri)) {
+                val id = DocumentsContract.getDocumentId(uri)
+                val contentUri = ContentUris.withAppendedId(
+                    Uri.parse("content://downloads/public_downloads"), java.lang.Long.valueOf(id)
+                )
+                return getDataColumn(context, contentUri, null, null)
+            } else if (isMediaDocument(uri)) {
+                val docId = DocumentsContract.getDocumentId(uri)
+                val split = docId.split(":".toRegex()).toTypedArray()
+                val type = split[0]
+                var contentUri: Uri? = null
+                if ("image" == type) {
+                    contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+                } else if ("video" == type) {
+                    contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI
+                } else if ("audio" == type) {
+                    contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI
+                }
+                val selection = "_id=?"
+                val selectionArgs = arrayOf(
+                    split[1]
+                )
+                return getDataColumn(context, contentUri, selection, selectionArgs)
+            }
+        } else if ("content".equals(uri.scheme, ignoreCase = true)) {
+
+            // Return the remote address
+            return if (isGooglePhotosUri(uri)) uri.lastPathSegment else getDataColumn(
+                context,
+                uri,
+                null,
+                null
+            )
+        } else if ("file".equals(uri.scheme, ignoreCase = true)) {
+            return uri.path
+        }
+        return null
+    }
+
+    fun getDataColumn(
+        context: Context, uri: Uri?, selection: String?,
+        selectionArgs: Array<String>?
+    ): String? {
+        var cursor: Cursor? = null
+        val column = "_data"
+        val projection = arrayOf(
+            column
+        )
+        try {
+            cursor = uri?.let {
+                context.getContentResolver().query(
+                    it, projection, selection, selectionArgs,
+                    null
+                )
+            }
+            if (cursor != null && cursor.moveToFirst()) {
+                val index: Int = cursor.getColumnIndexOrThrow(column)
+                return cursor.getString(index)
+            }
+        } finally {
+            if (cursor != null) cursor.close()
+        }
+        return null
+    }
+
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is ExternalStorageProvider.
+     */
+    fun isExternalStorageDocument(uri: Uri): Boolean {
+        return "com.android.externalstorage.documents" == uri.authority
+    }
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is DownloadsProvider.
+     */
+    fun isDownloadsDocument(uri: Uri): Boolean {
+        return "com.android.providers.downloads.documents" == uri.authority
+    }
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is MediaProvider.
+     */
+    fun isMediaDocument(uri: Uri): Boolean {
+        return "com.android.providers.media.documents" == uri.authority
+    }
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is Google Photos.
+     */
+    fun isGooglePhotosUri(uri: Uri): Boolean {
+        return "com.google.android.apps.photos.content" == uri.authority
+    }
+
 }
